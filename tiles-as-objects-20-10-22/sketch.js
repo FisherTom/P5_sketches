dc11b7738a7f20a3cab9a10e80a12839fcfa6c9c
TommyTank92 <tfisher92@gmail.com>
2022-11-06 13:42:34 +0000

    template init

--- tiles-as-objects-20-10-22/sketch.js
+++ tiles-as-objects-20-10-22/sketch.js
@@ -1,11 +1,38 @@
-def setup();
-{
-createCanvas(1000,1000);
-background(100)
+let url = 'https://coolors.co/709255-3e5622-83781b-172815-dff3e4';
+const createPalette = url => {
+const slash_index = url.lastIndexOf('/');
+const palette_str = url.slice(slash_index + 1);
+const arr = palette_str.split('-');
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = `#${arr[i]}`;
+}
+return arr;
+}
+const palette = createPalette(url);
+
+function setup() {
+createCanvas(1000, 1000); //SVG
+strokeWeight(0.5)
+noStroke()
+background(230)
+blendMode(MULTIPLY)
+colorMode(HSB);
+document.getElementById("bod").style.background = palette[palette.length -1];
 }
 
-def draw();
+function draw() 
 {
+noLoop()
+}
 
-    
+function tile(){
+  
+}
+
+function keyTyped() {
+    if (key === 's') {
+  save("mySVG.svg"); // give file name
+  print("saved svg");
+  noLoop(); // we just want to export once
+  }
 }
