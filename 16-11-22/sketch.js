d504f63a3f959f903829f31f546900c53e702125
TommyTank92 <tfisher92@gmail.com>
2022-11-20 00:52:36 +0000

    working on sketch 16-11-22

--- 16-11-22/sketch.js
+++ 16-11-22/sketch.js
@@ -12,6 +12,8 @@ const createPalette = url => {
 
 function windowResized(){
   resizeCanvas(windowWidth,windowHeight)
+  tiles = []
+  createTiles()
 }
 
   // colour palette from coolors.co URL
@@ -19,29 +21,60 @@ const palette = createPalette('https://coolors.co/palette/eddcd2-fff1e6-fde2e4-f
 let canvas;
 
 class Tile{
-  constructor(x,y,size){
+  constructor(x,y,size = res){
     this.x = x
     this.y = y
+    this.xorig = x
+    this.yorig = y
     this.size = size
   }
 
   show(){
-    rect(this.x,this.y,this.size)
+    circle(this.x,this.y,this.size)
+  }
+
+  noiseTranslate(j,t){
+    this.x = this.xorig+ j * cos(noise(0,this.y/noiseDiv,t))
+    this.y = this.yorig+ j * sin(noise(this.x/noiseDiv,0,t))
+  }
+
+  mouseTranslate(){
+  
   }
 }
+
+////////global variables////////
+let border = 100
+let res = 100; //canvas division
+let tiles = [];
+let t =1;
+let noiseDiv = 500;
+///////////////////////////////
 
 function setup() {
   canvas = createCanvas(windowWidth,windowHeight)
   canvas.position(0,0)
   windowResized()
   canvas.style('z-index','-1') // behind html elements
+  //noStroke()
+  fill(palette[9])
 }
 
-let tile = new Tile(100,100,100)
-
 function draw() {
-  background(palette[3]);
-  tile.show()
+  translate(-350,-180)
+  background(palette[1]);
+  tiles.map(tile => tile.size = 100*noise(tile.x/500,tile.y/100,t))
+  tiles.map(tile => tile.noiseTranslate(400,t))
+  tiles.map(tile => tile.show())
+  t+=0.005
+}
+
+function createTiles(){
+  for(let y=border; y<height-border; y+=res){
+    for(let x=border; x<width-border; x+=res){
+      tiles.push(new Tile(x,y))
+    }
+  }
 }
 
 
