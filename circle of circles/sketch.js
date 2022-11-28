a1c771f1462748fee14358b48aa877a14336c5cf
TommyTank92 <tfisher92@gmail.com>
2022-11-28 13:40:18 +0000

    edit page styling

--- circle of circles/sketch.js
+++ circle of circles/sketch.js
@@ -1,20 +1,29 @@
 const angles = 0.015;
 let t=0
 
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight)
+}
+
 function setup() {
-  createCanvas(800, 800);
+  canvas = createCanvas(windowWidth,windowHeight)
+  canvas.position(0,0)
+  windowResized()
+  canvas.style('z-index','-1') // behind html elements
+  
   strokeWeight(0.3);
   stroke(0); 
   noFill();
-  spac = width / 2
+  spacx = width / 2
+  spacy = height / 2
 }
 
 function draw() {
   background(240,255,255);
   for(let a = 0; a < TWO_PI; a += angles)
   {
-    let x = spac+cos(a)*200 
-    let y = spac+sin(a)*200 
+    let x = spacx+cos(a)*200 
+    let y = spacy+sin(a)*200 
     let xoff = cos(a+t)/2 + 20 ;
     let yoff = sin(a+t)/2 + 20 ; // try tan - sin
     let r = map(noise(xoff,yoff,t),0,1,-500,500);
