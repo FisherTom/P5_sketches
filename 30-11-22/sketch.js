122c656ccad7c5d6e4ec955fb7634fed22ec1334
TommyTank92 <tfisher92@gmail.com>
2022-11-30 23:29:02 +0000

    improve latest sketch

--- 30-11-22/sketch.js
+++ 30-11-22/sketch.js
@@ -24,11 +24,48 @@ function setup() {
   canvas.position(0,0)
   windowResized()
   canvas.style('z-index','-1') // behind html elements
+  background(palette[3])
+
+  b = new Ball(); //we create our b instance of Ball;
 }
+
+
+let margin = 30
 
 function draw() {
-  background(palette[3]);
+  //background(palette[3]);
+  b.update(); 
+  b.display();
 }
 
+////////////////////////////////////////
+
+class Ball {
+  constructor() {
+    this.position = new createVector(random(width), random(height));
+    this.velocity = new createVector(random(-5,8), random(-2,12));
+    this.dia = random(50,300)
+  }
+  
+  update() {
+    // Add the current speed to the position.
+    this.position.add(this.velocity);
+
+    
+    ////////////  Wall Rebound conditions //////////// NEEDS FIXING
+    if ((this.position.x > width-(this.dia/2 -margin)) || (this.position.x < 0+ this.dia/2+margin)) {
+      this.velocity.x = this.velocity.x * -1;
+    }
+    if ((this.position.y > height-(this.dia/2 -margin)) || (this.position.y < 0 + this.dia/2+margin)) {
+      this.velocity.y = this.velocity.y * -1;
+    }
+    this.dia = noise(this.position.x/400, this.position.y/400)*800 - 100
+  }
+
+  display() {
+    // Display circle at x position
+    circle(this.position.x, this.position.y, this.dia);
+  }
+}
 
 
--- sea-of-spots/sketch.js
+++ sea-of-spots/sketch.js
@@ -42,7 +42,7 @@ class Tile{
 
 ////////global variables////////
 let border = -200
-let res = 90; //canvas division
+let res = 80; //canvas division
 let tiles = [];
 let t =1;
 let noiseDiv = 2000;
@@ -56,13 +56,13 @@ function setup() {
   noStroke()
   fill(palette[9])
   colorMode(HSB)
-  blendMode(DIFFERENCE)
+  //blendMode(DIFFERENCE)
   background(0);
 }
 
 function draw() {
   translate(-200,-300)
-  clear()
+  //clear()
   //background(0);
   tiles.map(tile => tile.size = 150*noise(tile.x/noiseDiv,tile.y/noiseDiv,t))
   tiles.map(tile => tile.noiseTranslate(400,t))
