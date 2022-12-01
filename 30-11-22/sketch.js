5a8a37e04e8749a73efc6679a1c17700ab1ae092
TommyTank92 <tfisher92@gmail.com>
2022-12-01 00:02:17 +0000

    improve most recent sketch

--- 30-11-22/sketch.js
+++ 30-11-22/sketch.js
@@ -25,12 +25,14 @@ function setup() {
   windowResized()
   canvas.style('z-index','-1') // behind html elements
   background(palette[3])
+  fill(palette[9])
+  stroke(palette[1])
 
   b = new Ball(); //we create our b instance of Ball;
 }
 
 
-let margin = 30
+let margin = 200
 
 function draw() {
   //background(palette[3]);
@@ -41,25 +43,32 @@ function draw() {
 ////////////////////////////////////////
 
 class Ball {
-  constructor() {
-    this.position = new createVector(random(width), random(height));
+  constructor(t=0) {
+    this.position = new createVector(random(0+margin,width-margin), random(0+margin, height-margin));
     this.velocity = new createVector(random(-5,8), random(-2,12));
-    this.dia = random(50,300)
+    this.dia = random(50,200)
+    this.t = t
   }
   
   update() {
-    // Add the current speed to the position.
+    // need to figure out correct velocity calc for mouse movment
+    // this.velocity.x = -mouseX/200
+    // this.velocity.y = -mouseY/200
+
+     // Add the current speed to the position.
     this.position.add(this.velocity);
 
     
     ////////////  Wall Rebound conditions //////////// NEEDS FIXING
-    if ((this.position.x > width-(this.dia/2 -margin)) || (this.position.x < 0+ this.dia/2+margin)) {
+    if ((this.position.x > width -margin) || (this.position.x < 0+margin)) {
       this.velocity.x = this.velocity.x * -1;
     }
-    if ((this.position.y > height-(this.dia/2 -margin)) || (this.position.y < 0 + this.dia/2+margin)) {
+    if ((this.position.y > height -margin) || (this.position.y < 0 +margin)) {
       this.velocity.y = this.velocity.y * -1;
     }
-    this.dia = noise(this.position.x/400, this.position.y/400)*800 - 100
+    
+    this.dia = noise(this.position.x/400, this.position.y/400,this.t)*600 - 150
+    this.t+=0.001
   }
 
   display() {
