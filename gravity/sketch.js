f85caf463b4bbe64ee907c6764c85c1cb8f03b5e
TommyTank92 <tfisher92@gmail.com>
2022-12-12 22:05:00 +0000

    sketch improvments

--- gravity/sketch.js
+++ gravity/sketch.js
@@ -17,7 +17,6 @@ function windowResized(){
  // colour palette from coolors.co URL
 const palette = createPalette('https://coolors.co/palette/eddcd2-fff1e6-fde2e4-fad2e1-c5dedd-dbe7e4-f0efeb-d6e2e9-bcd4e6-99c1de');
 let canvas;
-let numberOfBalls = 500
 
 let balls = []
 
@@ -28,17 +27,19 @@ function setup() {
   canvas.style('z-index','-1') // behind html elements
  
   fill(100,50,50)
-  stroke(palette[1])
+  noStroke()
   colorMode(HSB,100)
-  blendMode(MULTIPLY)
+  //blendMode(MULTIPLY)
 }
 
 let margin = 200
 
 function draw() {
-  //background(0,0,1000,10);
-  clear()
-  circle(width/2,height/2,200)
+  background(0,0,0,40);
+  //clear()
+  fill(50)
+  noStroke()
+  circle(width/2,height/2,10)
 
 
   for(ball in balls){
@@ -48,7 +49,7 @@ function draw() {
 }
 
 function mousePressed() {
-  balls.push(new Ball(mouseX,mouseY,random(10,100),0));
+  balls.push(new Ball(mouseX,mouseY,random(5,50),0));
   // prevent default
   return false;
 }
@@ -58,34 +59,28 @@ class Ball {
     this.pos = createVector(x,y);
     this.velocity = createVector(random(-5,5),random(-5,5));
     this.acc = createVector()
-    this.acc.setMag(0.1) 
+    this.acc.setMag(0.3) 
     this.dia = d
-    this.color = c
+    this.color = random(0,100)
+    this.centerOfGravity = createVector(width/2,height/2);
   }
   
   update() {
-    let mouse = createVector(width/2,height/2); // get mouse vector     *********OVERRIDE*******
-    this.acc = p5.Vector.sub(mouse, this.pos) // acceleration = mouse - position
+   
+    this.acc = p5.Vector.sub(this.centerOfGravity, this.pos) // acceleration = mouse - position
     this.acc.setMag(0.06)          // limit acceleration changes responsiveness
     this.velocity.add(this.acc)           //velocity = velocity + acceleration
     this.velocity.limit(20)               // limmit max velocity
     this.pos.add(this.velocity);    // position = position +velocity
-
-    
-    ////////////  Wall Rebound conditions //////////// 
-    // if ((this.pos.x > width -margin) || (this.pos.x < 0+margin)) {
-    //   this.velocity.x = this.velocity.x * -1;
-    // }
-    // if ((this.pos.y > height -margin) || (this.pos.y < 0 +margin)) {
-    //   this.velocity.y = this.velocity.y * -1;
-    // }
-    
   }
 
   display() {
     // Draws the circle
-    stroke(this.color,50,100)
+    fill(this.color,50,100)
+    noStroke()
     circle(this.pos.x, this.pos.y, this.dia);
+    stroke(this.color,50,100)
+    line(this.pos.x,this.pos.y,this.centerOfGravity.x,this.centerOfGravity.y)
   }
 }
 
